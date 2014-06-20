1ac9d2c22502acd6e9483087a2cf92f0b0be35d9
prokilogrammer <sanath@metromile.com>
2014-06-19 23:16:32 -0700

    Making Backbone store/retrieve models with backend via APIs

--- main/api.py
+++ main/api.py
@@ -5,12 +5,18 @@ from tastypie.authorization import Authorization
 
 from main.models import Login, UserProfile, Contact
 
-class ContactResource(ModelResource):
+# Wrapper to make models sent by Tastypie to be compatible with Backbone.js
+# REF: http://paltman.com/2012/04/30/integration-backbonejs-tastypie/
+class BackboneCompatibleResource(ModelResource):
+  class Meta:
+    always_return_data = True
+
+class ContactResource(BackboneCompatibleResource):
   class Meta:
     queryset = Contact.objects.all()
     resource_name = 'contact'
 
-class UserProfileResource(ModelResource):
+class UserProfileResource(BackboneCompatibleResource):
   contact = fields.ForeignKey(ContactResource, 'contact', full=True)
 
   class Meta:
@@ -18,14 +24,14 @@ class UserProfileResource(ModelResource):
     resource_name = 'user_profile'
 
 
-class UserResource(ModelResource):
+class UserResource(BackboneCompatibleResource):
   class Meta:
     queryset = User.objects.all()
     resource_name = 'user'
     fields = ['username']
     allowed_methods = ['get']
 
-class LoginResource(ModelResource):
+class LoginResource(BackboneCompatibleResource):
   user = fields.ForeignKey(UserResource, 'user', full=True)
   user_profile = fields.ForeignKey(UserProfileResource, 'user_profile', full=True)
 
